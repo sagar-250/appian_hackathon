22c42c236e8906305e900d9dd96c74ac4777ddb1
sagar-250 <146671240+sagar-250@users.noreply.github.com>
2024-12-23 20:25:59 +0530

    Update extracter.py

--- extracter.py
+++ extracter.py
@@ -3,14 +3,16 @@ import base64
 from ocr import image_to_text
 
 def extract_text_and_images_info(input_path):
-    if input_path.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff','.webp')):
+    file_name = input_path.split('/')[-1]  # Extract the file name from the path
+    if input_path.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff', '.webp')):
         with open(input_path, "rb") as image_file:
             image_bytes = image_file.read()
             image_base64 = base64.b64encode(image_bytes).decode("utf-8")
-            return image_to_text(image_base64)
+            text = image_to_text(image_base64)
+            return f"{file_name}\n{text}"
     elif input_path.lower().endswith('.pdf'):
         pdf_file = fitz.open(input_path)
-        all_text = ""
+        all_text = f"file name:{file_name}\n"  # Initialize with the file name
         for page_index in range(len(pdf_file)):
             page = pdf_file.load_page(page_index)
             all_text += page.get_text() + "\n"
@@ -25,5 +27,3 @@ def extract_text_and_images_info(input_path):
         return all_text
     else:
         raise ValueError("Unsupported file type. Please provide a PDF or image file.")
-
-
