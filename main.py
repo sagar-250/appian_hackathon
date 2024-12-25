7a586efb025aa9ea480ad57f18ce26b78ddd0b8f
sagar-sarvam <sagar@sarvam.ai>
2024-12-26 00:44:26 +0530

    ll

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from extracter import extract_text_and_images_info
 from classifier import classifier_summerizer
 from process_text import process
-extracted_text=extract_text_and_images_info("samples/img6.jpeg")
+extracted_text=extract_text_and_images_info("samples\Sagar's_recipt.pdf")
 print(extracted_text)
 cleaned_text=process(extracted_text)
 
